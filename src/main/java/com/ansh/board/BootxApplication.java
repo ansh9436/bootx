d99a9706dc07043b53548c53784c83e466840233
ansh <ansh9436@gmail.com>
2021-01-31 23:38:58 +0900

    Initial commit

---
+++ src/main/java/com/ansh/board/BootxApplication.java
@@ -0,0 +1,13 @@
+package com.ansh.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ansh/board/BootxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansh.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
