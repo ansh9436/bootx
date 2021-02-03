1acbc8e4168fea299c865c4ba70d19a5b596fc30
안승호 <ansh@cnspartner.com>
2021-02-03 14:50:25 +0900

    업로드테스

--- src/main/java/com/ansh/board/repository/BoardRepository.java
+++ src/main/java/com/ansh/board/repository/BoardRepository.java
@@ -7,5 +7,4 @@ import com.ansh.board.model.BoardVO;
 
 @RepositoryRestResource(collectionResourceRel = "board", path = "board")
 public interface BoardRepository extends JpaRepository<BoardVO, Long> {
-
 }
