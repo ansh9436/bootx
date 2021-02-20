f96b8e0f729a2c4595a97c82a9a5619059c9bf0e
ansh <ansh@localhost>
2021-02-20 19:34:08 +0900

    일단돌아감

--- src/main/java/com/ansh/board/controller/BoardController.java
+++ src/main/java/com/ansh/board/controller/BoardController.java
@@ -22,10 +22,13 @@ public class BoardController {
 
 
     @GetMapping("/")
-    public String list(Model model) {
-        List<BoardDTO> boardList = boardService.getBoardlist();
+    public String list(Model model, @RequestParam(value="page", defaultValue="1") Integer pageNum) {
+        List<BoardDTO> boardList = boardService.getBoardlist(pageNum);
+        Integer[] pageList = boardService.getPageList(pageNum);
 
         model.addAttribute("boardList", boardList);
+        model.addAttribute("pageList", pageList);
+        
         return "board/list.html";
     }
 
--- src/main/java/com/ansh/board/service/BoardService.java
+++ src/main/java/com/ansh/board/service/BoardService.java
@@ -4,6 +4,10 @@ import com.ansh.board.model.BoardDTO;
 import com.ansh.board.model.BoardVO;
 import com.ansh.board.repository.BoardRepository;
 import lombok.AllArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,24 +20,52 @@ import javax.transaction.Transactional;
 @Service
 public class BoardService {
     private BoardRepository boardRepository;
+    
+    private static final int BLOCK_PAGE_NUM_COUNT = 5;  // 블럭에 존재하는 페이지 번호 수
+    private static final int PAGE_POST_COUNT = 5;       // 한 페이지에 존재하는 게시글 수
 
-    @Transactional
-    public List<BoardDTO> getBoardlist() {
-        List<BoardVO> boardVOList = boardRepository.findAll();
+    public List<BoardDTO> getBoardlist(Integer pageNum) {
+    	Page<BoardVO> page = boardRepository.findAll(PageRequest.of(pageNum - 1, 
+    										PAGE_POST_COUNT, Sort.by(Sort.Direction.ASC, "createdDate")));
+    	List<BoardVO> boardVOList = page.getContent();
         List<BoardDTO> boardDTOList = new ArrayList<>();
 
-        for(BoardVO boardVO : boardVOList) {
+        for(BoardVO vo : boardVOList) {
             BoardDTO boardDTO = BoardDTO.builder()
-                    .id(boardVO.getId())
-                    .title(boardVO.getTitle())
-                    .content(boardVO.getContent())
-                    .writer(boardVO.getWriter())
-                    .createdDate(boardVO.getCreatedDate())
+                    .id(vo.getId())
+                    .title(vo.getTitle())
+                    .content(vo.getContent())
+                    .writer(vo.getWriter())
+                    .createdDate(vo.getCreatedDate())
                     .build();
 
             boardDTOList.add(boardDTO);
         }
         return boardDTOList;
+    }
+    
+    public Integer[] getPageList(Integer curPageNum) {
+        Integer[] pageList = new Integer[BLOCK_PAGE_NUM_COUNT];
+
+        // 총 게시글 갯수
+        Double postsTotalCount = Double.valueOf(boardRepository.count());
+
+        // 총 게시글 기준으로 계산한 마지막 페이지 번호 계산 (올림으로 계산)
+        Integer totalLastPageNum = (int)(Math.ceil((postsTotalCount/PAGE_POST_COUNT)));
+
+        // 현재 페이지를 기준으로 블럭의 마지막 페이지 번호 계산
+        Integer blockLastPageNum = (totalLastPageNum > curPageNum + BLOCK_PAGE_NUM_COUNT)
+                ? curPageNum + BLOCK_PAGE_NUM_COUNT
+                : totalLastPageNum;
+
+        // 페이지 시작 번호 조정
+        curPageNum = (curPageNum <= 3) ? 1 : curPageNum - 2;
+
+        // 페이지 번호 할당
+        for (int val = curPageNum, idx = 0; val <= blockLastPageNum; val++, idx++) {
+            pageList[idx] = val;
+        }
+        return pageList;
     }
     
     @Transactional
@@ -62,7 +94,6 @@ public class BoardService {
         boardRepository.deleteById(id);
     }
     
-    @Transactional
     public List<BoardDTO> searchPosts(String keyword) {
         List<BoardVO> boardVOs = boardRepository.findByTitleContaining(keyword);
         List<BoardDTO> boardDTOList = new ArrayList<>();
